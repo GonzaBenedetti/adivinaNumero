09303e7995982c159851846dd6e86060d292cdb6
GonzaBenedetti <143021859+GonzaBenedetti@users.noreply.github.com>
2023-10-23 14:41:37 -0300

    Agregado de boton reset y correciones de usuario.

--- js/main.js
+++ js/main.js
@@ -4,29 +4,41 @@ let mensaje = document.getElementById(`mensaje`);
 let prueba = document.getElementById(`prueba`);
 let pruebas = 0;
 
+
 function chequearNumero () {
     pruebas ++;
     prueba.textContent = pruebas;
     let numeroEntrada = parseInt(numeroIngresado.value);
 
     if (numeroEntrada < 1 || numeroEntrada > 100 || isNaN(numeroEntrada)) {
-        mensaje.textContent = `Los datos ingresados no son validos. Vuelva a ingresar un numero entre el 1 y el 100`
+        mensaje.textContent = `El dato ingresado "${numeroEntrada}" no es valido. Vuelva a ingresar un numero entre el 1 y el 100.`
+        document.getElementById(`numeroIngresado`).value = ``;
         mensaje.style.color = `red`;
         return
     };
-
+    
     if(numeroEntrada === numero) {
-        mensaje.textContent = `FELICIDADES!! ADIVINASTE EL NUMERO.`;
+        mensaje.textContent = `FELICIDADES!! EL NUMERO GANADOR ES ${numero}.`;
         mensaje.style.color = `red`;
         mensaje.style.fontWeight = `bold`;
-        numeroIngresado.disable = true;
+        numeroIngresado.disabled = true;
 
     }else if (numeroEntrada > numero) {
-        mensaje.textContent = `¨Prueba con otro numero mas bajo`;
+        mensaje.textContent = `Prueba con otro numero mas bajo que ${numeroEntrada}.`;
+        document.getElementById(`numeroIngresado`).value = ``;
         mensaje.style.color = `red`;
-
+        
     }else {
-        mensaje.textContent = `Prueba con otro numero mas alto`;
+        mensaje.textContent = `Prueba con otro numero mas alto que ${numeroEntrada}.`;
+        document.getElementById(`numeroIngresado`).value = ``;
         mensaje.style.color = `red`;
     }
+}
+function resetNumero(){
+    pruebas = 0;
+    prueba.textContent = pruebas;
+    numero = Math.floor(Math.random()*100) + 1;
+    numeroIngresado.disabled = false;
+    document.getElementById(`numeroIngresado`).value = ``;
+    mensaje.textContent = `Comenza a jugar.`
 }
